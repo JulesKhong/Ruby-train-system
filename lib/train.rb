9a4f74d35cb130a0b061a15014b3225486e03b44
Julia Dickey and Noah Yasskin <noah@>
2016-07-13 15:01:14 -0700

    pass spec that shows a train as being the same when they share the same id and name

--- lib/train.rb
+++ lib/train.rb
@@ -12,11 +12,19 @@ class Train
     returned_trains = DB.exec("SELECT * FROM trains;")
     trains = []
     returned_trains.each() do |train|
+      id = train.fetch('id').to_i()
       name = train.fetch('name')
-      id = train.fetch('id')
       trains.push(Train.new({:name => name, :id => id}))
     end
     trains
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO trains (name) VALUES ('#{@name}')")
+  end
+
+  define_method(:==) do |another_train|
+    self.name().==(another_train.name()).&(self.id().==(another_train.id()))
   end
 
 end
--- spec/trian_spec.rb
+++ spec/trian_spec.rb
@@ -14,11 +14,29 @@ RSpec.configure do |config|
 end
 
 describe(Train) do
+
   describe('.all') do
     it("shows the train array as empty at first") do
       expect(Train.all()).to(eq([]))
     end
   end
 
+ describe('#==') do
+   it("shows a train as being the same when they share the same id and name") do
+     test_train = Train.new({:id => nil, :name => "Thomas"})
+     test_train1 = Train.new({:id => nil, :name => "Thomas"})
+     expect(test_train).to(eq(test_train1))
+   end
+ end
+
+
+  describe('#save') do
+    it("saves a new train object in the database") do
+      test_train = Train.new({:id => nil, :name => 'Thomas'})
+      test_train.save()
+  binding.pry
+      expect(Train.all()).to(eq[test_train])
+    end
+  end
 
 end
